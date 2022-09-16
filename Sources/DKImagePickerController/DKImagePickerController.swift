eac77c69e15d059f46d0a38575621687e5391a23
Athipat Nampetch <athipat@ltman.com>
2022-09-16 10:55:07 +0700

    Fix for Xcode 14

--- Sources/DKImagePickerController/DKImagePickerController.swift
+++ Sources/DKImagePickerController/DKImagePickerController.swift
@@ -65,11 +65,8 @@ open class DKImagePickerController: DKUINavigationController, DKImageBaseManager
     }
     
     /// false to prevent dismissal of the picker when the presentation controller will dismiss in response to user action.
-    @available(iOS 13.0, *)
-    @objc public var shouldDismissViaUserAction: Bool {
-        return false
-    }
-
+    @objc lazy public var shouldDismissViaUserAction = false
+    
     /// Forces deselect of previous selected image. allowSwipeToSelect will be ignored.
     @objc public var singleSelect = false
     
